608bf36682ff69d59477c561f0b08bf8426afc48
Pedro Ilidio <pedro.ilidio@kuleuven.be>
2025-07-21 18:46:44 +0200

    server start command

--- experiments/command.sh
+++
@@ -1,9 +0,0 @@
-python scripts/run_experiments.py \
-    --estimator-definitions config/estimators.yml \
-    --dataset-definitions config/datasets.yml \
-    --fold-definitions config/fold_definitions.yml \
-    --experiment-definitions config/experiments.yml \
-    --scoring-definitions config/scoring.yml \
-    --output-directory mlruns \
-    --n-jobs 5 \
-    --code-path ./
---
+++ experiments/start_mlflow_server.sh
@@ -0,0 +1,5 @@
+mlflow server \
+    --host 0.0.0.0 \
+    --port 5000 \
+    --backend-store-uri "sqlite:///mlruns.db" \
+
